0332517e9d247ec5c0e2d9d63d7a76342e3f9d86
nejni-marji <nejni.marji@gmail.com>
2018-03-29 17:35:17 -0500

    Added i3 bind logging

---
+++ i3/bin/bind_logging.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from i3ipc import Connection
+from subprocess import call
+
+i3 = Connection()
+
+def notify(i3, event):
+	bind = event.binding
+
+	mods_dict = [
+		['Mod4', 'Super'],
+		['shift', 'Shift'],
+		['ctrl', 'Control'],
+		['Mod1', 'Alt'],
+	]
+	mods = '+'.join([
+		mod[1]
+		for mod in mods_dict
+		if mod[0] in bind.mods
+	])
+	if mods: mods += '+'
+
+	print('%s%s: %s' % (mods, bind.symbol, bind.command))
+
+	return None
+	call('notify-send -t 1000 -a i3wm'.split(' ')
+		+ ['%s%s' % (mods, bind.symbol), bind.command]
+	)
+
+i3.on('binding', notify)
+
+i3.main()
