290544201a0eb48b75b7710695b5df22fbeac4ec
nejni-marji <nejni.marji@gmail.com>
2018-04-22 15:04:56 -0500

    Added command to swap away a sticky container

--- i3/bin/hotswap.py
+++ i3/bin/hotswap.py
@@ -6,6 +6,7 @@ i3 = i3ipc.Connection()
 
 re_uuid = '([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)'
 re_here = re_compile('^_?hs_[A-Za-z0-9]+_(?!0$)(\d+|%s)+$' % re_uuid).match
+re_this = re_compile('^_?hs_[A-Za-z0-9]+_0$').match
 
 def hotswap(workspace):
 	data = []
@@ -27,7 +28,7 @@ def on_workspace_focus(self, event):
 	workspace = event.current
 	hotswap(workspace)
 
-def on_window(self, event):
+def on_window_mark(self, event):
 	workspace = i3.get_tree().find_focused().workspace()
 	hotswap(workspace)
 
@@ -51,9 +52,27 @@ def on_binding(self, event):
 		command += '-F "mark hs_%s_$(uuidgen)"'
 		i3.command(command)
 		return None
+	if event.binding.command == 'nop ipc hotswap return':
+		focused = i3.get_tree().find_focused()
+		marks = [
+			i for
+			i in focused.marks
+			if re_this(i)
+		]
+		if not len(marks) == 1:
+			return None
+		mark = marks[0]
+		desc = mark.split('_')[-2]
+		command = '[con_mark="^hs_{desc}_(?!0$)({uuid}|\d+)$"] swap container with mark {mark}'.format(
+			desc = desc,
+			uuid = re_uuid,
+			mark = mark,
+		)
+		i3.command(command)
+		return None
 
 i3.on('workspace::focus', on_workspace_focus)
-i3.on('window', on_window)
+i3.on('window::mark', on_window_mark)
 i3.on('binding', on_binding)
 
 i3.main()
