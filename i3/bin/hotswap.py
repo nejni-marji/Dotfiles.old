448654ec6c7887eda8b33321fc1f9ff88ef0b7b7
nejni-marji <nejni.marji@gmail.com>
2018-04-22 14:18:21 -0500

    Tweaked namespaces in hotswap

--- i3/bin/hotswap.py
+++ i3/bin/hotswap.py
@@ -4,15 +4,15 @@ from re import compile as re_compile
 
 i3 = i3ipc.Connection()
 
-uuid = '([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)'
-regex = re_compile('^_?hs_[A-Za-z0-9]+_(?!0$)(\d+|%s)+$' % uuid).match
+re_uuid = '([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)'
+re_here = re_compile('^_?hs_[A-Za-z0-9]+_(?!0$)(\d+|%s)+$' % re_uuid).match
 
 def hotswap(workspace):
 	data = []
 	for con in workspace.descendents():
 		if not con.workspace().name == workspace.name: continue
 		if con.marks == []: continue
-		marks = [i for i in con.marks if regex(i)]
+		marks = [i for i in con.marks if re_here(i)]
 		if not len(marks) == 1: continue
 		mark = marks[0]
 		data.append(mark)
@@ -33,19 +33,19 @@ def on_window(self, event):
 
 def on_binding(self, event):
 	command = event.binding.command
-	if command == 'nop ipc hotswap swapthis':
+	if event.binding.command == 'nop ipc hotswap swapthis':
 		command  = 'exec i3-input '
 		command += '-P "hotswap swapthis " '
 		command += '-F "mark hs_%s_0"'
 		i3.command(command)
 		return None
-	if command == 'nop ipc hotswap swapthis hide':
+	if event.binding.command == 'nop ipc hotswap swapthis hide':
 		command  = 'exec i3-input '
 		command += '-P "hotswap swapthis " '
 		command += '-F "mark _hs_%s_0"'
 		i3.command(command)
 		return None
-	if command == 'nop ipc hotswap swaphere':
+	if event.binding.command == 'nop ipc hotswap swaphere':
 		command  = 'exec i3-input '
 		command += '-P "hotswap swaphere " '
 		command += '-F "mark hs_%s_$(uuidgen)"'
