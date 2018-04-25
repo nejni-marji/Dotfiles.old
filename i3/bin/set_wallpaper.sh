670e5c4fa6485a385ee9bdafcc200c70f6d21eed
nejni-marji <nejni.marji@gmail.com>
2018-04-25 14:12:38 -0500

    Changed wallpaper

--- i3/bin/set_wallpaper.sh
+++ i3/bin/set_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $(hostname) in
 	Desktop)
-		feh --bg-scale ~/.wallpaper/waluigi/too_bad_waluigi_time_1920x1080.png
+		feh --bg-scale ~/.wallpaper/astronomy/earth/usa_nighttime_1920x1080.png
 		;;
 	ASUS|Toshiba)
 		feh --bg-scale ~/.wallpaper/ftl/kestrel_by_andrewcolunga_1366x768.png
