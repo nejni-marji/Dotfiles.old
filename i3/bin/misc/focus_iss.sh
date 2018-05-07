8ae2d382912a63929da5643477afd799934129af
nejni-marji <nejni.marji@gmail.com>
2018-05-06 21:02:45 -0500

    Added bindings to handle the HDEV stream window

---
+++ i3/bin/misc/focus_iss.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+iss='^ISS HD Earth Viewing Experiment on USTREAM: \. Science - Mozilla Firefox$'
+i3-msg '[title="'"$iss"'"] focus, fullscreen'
---
+++ i3/bin/misc/reset_iss.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+iss='^ISS HD Earth Viewing Experiment on USTREAM: \. Science - Mozilla Firefox$'
+xdotool search --name "$iss" key F5
