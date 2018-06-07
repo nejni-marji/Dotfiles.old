edbbf17dc54a450442f6d16aad540f12be0529b7
nejni-marji <nejni.marji@gmail.com>
2018-06-06 19:15:56 -0500

    Tweaked m570_sensitivity.sh

--- extras/bin/m570_sensitivity.sh
+++ extras/bin/m570_sensitivity.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 MATRIX="$1"
-[[ -z $MATRIX ]] && MATRIX=0.15
+[[ -z $MATRIX ]] && MATRIX=0.2
 ACCEL="$2"
-[[ -z $ACCEL ]] && ACCEL=0
+[[ -z $ACCEL ]] && ACCEL=1
 
 xinput set-prop Logitech\ M570 'Coordinate Transformation Matrix' $MATRIX 0 0 0 $MATRIX 0 0 0 1
 xinput set-prop Logitech\ M570 'libinput Accel Speed' $ACCEL
+
+xinput set-prop Logitech\ M570 'libinput Scroll Method Enabled' 0 0 1
+xinput set-prop Logitech\ M570 'libinput Button Scrolling Button' 9
