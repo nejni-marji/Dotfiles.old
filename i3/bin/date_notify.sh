6d4ed2b1271e8103256c9319c0ff0234844c889c
nejni-marji <nejni-marji@gmail.com>
2019-01-18 20:33:12 -0600

    Added fuzzyclock to i3/bin/date_notify.sh

--- i3/bin/date_notify.sh
+++ i3/bin/date_notify.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+if [[ $1 == "fuzzy" ]]; then
+	TIME="$(~/.i3/i3blocks.d/fuzzyclock | tail -n1 | rev | cut -c2- | rev)"
+else
+	TIME="$(date '+%T')"
+fi
+
+# "$(date '+%T')" \
+
 notify-send \
 -a i3wm \
 -t 5000 \
-"$(date '+%T')" \
+"$TIME" \
 "$(date '+%A, %B %-d')" \
